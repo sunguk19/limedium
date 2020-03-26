d5a1e23d8749befef1c1ea8e435bd78e557001d0
sunguk19 <sunguk19@naver.com>
2020-03-26 16:19:14 +0900

    z

--- src/main/java/com/example/limedium/springboot/web/PostsApiController.java
+++ src/main/java/com/example/limedium/springboot/web/PostsApiController.java
@@ -1,11 +1,11 @@
 package com.example.limedium.springboot.web;
 
 import com.example.limedium.springboot.service.post.PostsService;
+import com.example.limedium.springboot.web.dto.PostsResponseDto;
 import com.example.limedium.springboot.web.dto.PostsSaveRequestDto;
+import com.example.limedium.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RequiredArgsConstructor
@@ -17,5 +17,15 @@ public class PostsApiController {
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
     }
 }
