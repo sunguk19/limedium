62d66af95c6fa933f9fbc20c13dee3aa3dfefe90
sunguk19 <sunguk19@naver.com>
2020-03-27 11:22:34 +0900

    z

--- src/main/java/com/example/limedium/springboot/Application.java
+++ src/main/java/com/example/limedium/springboot/Application.java
@@ -1,9 +1,11 @@
 package com.example.limedium.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import javax.swing.*;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class Application {
     public static void main(String[] args){
--- src/main/java/com/example/limedium/springboot/domain/posts/Posts.java
+++ src/main/java/com/example/limedium/springboot/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.example.limedium.springboot.domain.posts;
 
+import com.example.limedium.springboot.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,7 +14,7 @@ import javax.persistence.Id;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
